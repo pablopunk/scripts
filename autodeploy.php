600d5dfa069d8941540f1cdc9fa119898a2c5e4e
Pablo Varela <pablovarela182@gmail.com>
2016-04-19 02:32:11 +0200

    automatic deploy php

---
+++ autodeploy.php
@@ -0,0 +1,35 @@
+<?php
+
+// Copyright (c) Pablo Varela 2016
+//
+// This file allows automatic deploy
+// of a git repo in your server.
+// Place the script in your server
+// so the webhook can access it
+
+$secret = 'puthereyourverysecretstring'; // CHANGE THIS
+$deploy_dir = '/var/www/'; // Root directory
+$branch = 'master'; // Branch to deploy
+
+// print new msg to log file
+function logs($msg) {
+    $log_file = 'autodeploy.log';
+    $msg = date('Y-m-d H:i:s -> ').$msg."\n";
+    file_put_contents ( $log_file , $msg, FILE_APPEND | LOCK_EX );
+}
+
+// get parameters
+if(! isset($_GET["sat"])) {
+    logs('Request with no parameter');
+    exit(1);
+} else if ( trim($_GET["sat"]) != $secret ) {
+    logs('Request with wrong parameter: '.trim($_GET["sat"]));
+    exit(1);
+}
+
+// all fine
+logs('Attempting autodeploy...');
+
+chdir($deploy_dir); // cd dir
+exec('git checkout origin/'.$branch.' -- . && git pull origin '.$branch); // pull
+
