b1d888ea4f22a9440131acbac27e59749c61b70a
Pablo Varela <pablovarela182@gmail.com>
2016-05-11 20:36:16 +0200

    fixed number of lines and added directory option

--- number_of_lines.sh
+++ number_of_lines.sh
@@ -4,9 +4,22 @@
 # Prints how many lines the current directory files have
 #
 
+if [ ! $# -eq 1 ]; then
+  echo "Usage ./script.sh path"
+  exit 1
+fi 
+
+dir="$1" # path
+if [ ! -d $dir ]; then
+  echo "$dir does not exist"
+  exit 1
+fi
+
+
 exclude="*.txt\|main\|*.m\|*.dSYM\|.*" # put here every file or regex you want NOT to be counted. Separate files or regex with \|
 
-ls -1 | grep -v $exclude | xargs wc -l | xargs echo | rev | cut -d' ' -f2
+cd $dir
+ls -1 | grep -v "$exclude" | xargs wc -l | xargs echo | rev | cut -d' ' -f2
 
 # ls -1 (list directory files, one line each)
 # grep -v $exclude (exclude all files you don't want)
