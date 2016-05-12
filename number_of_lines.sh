acdddadab9119bc2db8205c36408607f9c1533d8
Pablo Varela <pablovarela182@gmail.com>
2016-05-12 20:09:28 +0200

    a bit less SHIT on this SHITTY script

--- number_of_lines.sh
+++ number_of_lines.sh
@@ -4,22 +4,34 @@
 # Prints how many lines the current directory files have
 #
 
-if [ ! $# -eq 1 ]; then
-  echo "Usage ./script.sh path"
+if [ $# -lt 1 ]; then
+  echo "Usage: ./script.sh path [optional: --exclude <regexp>]"
+	echo "Example: ./number_of_lines.sh . -e \"*.txt\|main\|*.m\|*.dSYM\""
   exit 1
 fi 
 
-dir="$1" # path
-if [ ! -d $dir ]; then
-  echo "$dir does not exist"
-  exit 1
-fi
+dir="." # default dir
+default_exclude="*.txt\|main\|*.m\|*.dSYM" # default excluded files (separate regexp with \|)
+exclude="$default_exclude"
 
-
-exclude="*.txt\|main\|*.m\|*.dSYM\|.*" # put here every file or regex you want NOT to be counted. Separate files or regex with \|
+# args
+for ((i=0 ; i < $# ; i++)); do
+	if [ "$1" = "-e" ] || [ "$1" = "--exclude" ] ; then
+		if [ -z $2 ]; then
+			echo "Option -e|--exclude needs an argument"; exit 1;
+		else
+			exclude="$2"; shift; let i=$i+1 # jump arg
+		fi
+	elif [ -d $1 ] ; then # if directory exists
+		dir=$1
+	else
+		echo "Invalid argument: Directoy '$1' not found"; exit 1
+	fi
+	shift
+done
 
 cd $dir
-ls -1 | grep -v "$exclude" | xargs wc -l | xargs echo | rev | cut -d' ' -f2
+ls -1 | grep -v $exclude | xargs wc -l | xargs echo | rev | cut -d' ' -f2 | rev
 
 # ls -1 (list directory files, one line each)
 # grep -v $exclude (exclude all files you don't want)
