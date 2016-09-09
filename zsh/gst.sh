4b2bc020d47764e4d90bd342e5339dc4532301fa
Pablo Varela <pablovarela182@gmail.com>
2016-09-09 03:00:11 +0200

    red

--- zsh/gst.sh
+++ zsh/gst.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/zsh
-#
-# Copyright 2016 Pablo Varela
-# 
-# This script returns the current directory name
-# If it's a git repo, it will return the name but
-# with different colors, depending on the status
-#
-# It's used for the PROMPT variable in zshrc:
-# EXAMPLE:
-# PROMPT='$fg[red](%m) $(gst)> '
-#
 
 function getout {
 	echo -n $fg_no_bold[white]
@@ -26,7 +15,7 @@ repo=$(echo $directory | rev | cut -d'/' -f1 | rev)
 gitstatus=$(git status --porcelain | cut -d' ' -f2)
 
 [ -z $gitstatus ] && echo -n "$fg_bold[cyan]$repo" && getout
-[ $gitstatus = "D" ] && echo -n "$f_boldg[red]-$repo" && getout
+[ $gitstatus = "D" ] && echo -n "$f_bold[red]-$repo" && getout
 [ $gitstatus = "M" ] && echo -n "$fg_bold[yellow]·$repo" && getout
 echo -n "$fg_bold[green]$repo" && getout
 
