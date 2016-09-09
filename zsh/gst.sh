0fc31466cd2bb155cdf5dff6b0bedcb3509df88d
Pablo Varela <pablovarela182@gmail.com>
2016-09-08 21:25:02 -0400

    fix autocomplete

--- zsh/gst.sh
+++ zsh/gst.sh
@@ -6,14 +6,17 @@
 # If it's a git repo, it will return the name but
 # with different colors, depending on the status
 #
-# It's used for the PROMPT variable in zshrc:
-# EXAMPLE:
-# PROMPT='$fg[red](%m) $(gst)> '
+# It's used for the PROMPT variable in zshrc
+# EXAMPLE .zshrc:
+# 
+# setopt PROMPT_SUBST
+# autoload -Uz colors && colors
+# PROMPT='%{$fg[red]%}(%m) $(gst)> '
 #
 
 function getout {
-	echo -n $fg_no_bold[white]
-	echo -n $reset_color
+	echo -n %{$fg_no_bold[white]%}
+	echo -n %{$reset_color%}
 	exit 0
 }
 
@@ -21,13 +24,13 @@ autoload -Uz colors && colors
 directory=$(pwd)
 repo=$(echo $directory | rev | cut -d'/' -f1 | rev)
 
-[ ! -d .git ] && echo -n "$fg[blue]$repo" && getout
+[ ! -d .git ] && echo -n "%{$fg[blue]%}$repo" && getout
 
 gitstatus=$(git status --porcelain | cut -d' ' -f2)
 
-[ -z $gitstatus ] && echo -n "$fg_bold[cyan]$repo" && getout
-[ $gitstatus = "D" ] && echo -n "$fg_bold[red]-$repo" && getout
-[ $gitstatus = "M" ] && echo -n "$fg_bold[yellow]·$repo" && getout
-echo -n "$fg_bold[green]$repo" && getout
+[ -z $gitstatus ] && echo -n "%{$fg_bold[cyan]%}$repo" && getout
+[ $gitstatus = "D" ] && echo -n "%{$fg_bold[red]%}-$repo" && getout
+[ $gitstatus = "M" ] && echo -n "%{$fg_bold[yellow]%}·$repo" && getout
+echo -n "%{$fg_bold[green]%}$repo" && getout
 
 echo "WTF"
