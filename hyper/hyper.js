79d3378979cd9dac8ab06876a06923ff2225f4a8
Pablo Varela <pablovarela182@gmail.com>
2017-01-13 10:42:34 -0500

    hyper.js

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -1,31 +1,41 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 13,
+    fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: '"Source Code Pro for Powerline", "SF Mono", "Fira Code Light", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'UNDERLINE',
+    cursorShape: 'BLOCK',
 
     // color of the text
     foregroundColor: '#fff',
 
     // terminal background color
-    backgroundColor: 'rgba(0,0,0,1)',
+    backgroundColor: 'rgba(34,34,34,.7)',
 
     // border color (window, tabs)
-    borderColor: 'rgba(0,0,0,0)',
+    borderColor: '#333',
 
     // custom css to embed in the main window
     css: '',
 
     // custom css to embed in the terminal window
     termCSS: '',
+
+    // set to `true` if you're using a Linux set up
+    // that doesn't shows native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: '',
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: '12px 14px',
@@ -81,15 +91,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-    //'hyperterm-atom-dark',
-    //"hyper-dark-dracula",
-    //"hyper-simple-vibrancy"
-    //"spongebob"
-    //'hyperborder'
-    //'hyperpanic'
-    'hyper-criollo'
-  ],
+  plugins: [],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
