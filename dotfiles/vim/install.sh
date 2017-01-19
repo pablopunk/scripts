acdb9b002985880fb7c345561042ff289710f9be
Pablo Varela <pablovarela182@gmail.com>
2017-01-19 14:40:56 -0500

    Fix script to install dotfiles

--- dotfiles/vim/install.sh
+++ dotfiles/vim/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 file_name='vim.rc'
+dir=''
 
 function go_to_script_dir {
   pushd `dirname $0` > /dev/null
   dir=`pwd -P`
   popd > /dev/null
-  cd $dir
 }
 
 function link_config_file {
-  ln -svf ./$file_name ~/.$file_name
+  ln -svf $dir/$file_name ~/.$file_name
 }
 
 go_to_script_dir && link_config_file
--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,3 +4,6 @@ for D in `find dotfiles/ -mindepth 1 -type d`
 do
   bash $D/install.sh
 done
+
+ln -svf $PWD/dotfiles ~/.dotfiles
+
